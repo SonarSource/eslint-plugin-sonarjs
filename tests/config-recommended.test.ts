67507e0de8824563d290384ca5b1cf1eb2374b00
Stas Vilchik <stas.vilchik@sonarsource.com>
2018-04-05 11:00:42 +0200

    change index and config tests to dynamically explore rules (#21)

--- tests/config-recommended.test.ts
+++ tests/config-recommended.test.ts
@@ -1,5 +1,12 @@
+import * as fs from "fs";
+import * as path from "path";
 import config = require("../src/config-recommended");
 
-it("should declare rules", () => {
-  expect(Object.keys(config.rules)).toHaveLength(1);
+it("should declare all rules", () => {
+  const rulesPath = path.join(__dirname, "../src/rules");
+  const existingRules = fs.readdirSync(rulesPath).map(file => file.substring(0, file.indexOf(".ts")));
+  existingRules.forEach(rule => {
+    expect(config.rules).toHaveProperty(`sonarjs/${rule}`);
+  });
+  expect(Object.keys(config.rules)).toHaveLength(existingRules.length);
 });
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,9 +1,16 @@
+import * as fs from "fs";
+import * as path from "path";
 import { configs, rules } from "../src/index";
 
 it("should declare configs", () => {
   expect(configs.recommended).toBeDefined();
 });
 
-it("should declare rules", () => {
-  expect(Object.keys(rules)).toHaveLength(1);
+it("should declare all rules", () => {
+  const rulesPath = path.join(__dirname, "../src/rules");
+  const existingRules = fs.readdirSync(rulesPath).map(file => file.substring(0, file.indexOf(".ts")));
+  existingRules.forEach(rule => {
+    expect(rules).toHaveProperty(rule);
+  });
+  expect(Object.keys(rules)).toHaveLength(existingRules.length);
 });
