6c231d6576d5fe0b3817e3efe05b0b836c6745ca
Stas Vilchik <stas.vilchik@sonarsource.com>
2018-04-10 17:58:47 +0200

    add prefer-immediate-return rule

---
+++ docs/rules/prefer-immediate-return.md
@@ -0,0 +1,22 @@
+# prefer-immediate-return
+
+Declaring a variable only to immediately return or throw it is a bad practice.
+
+Some developers argue that the practice improves code readability, because it enables them to explicitly name what is being returned. However, this variable is an internal implementation detail that is not exposed to the callers of the method. The method name should be sufficient for callers to know exactly what will be returned.
+
+## Noncompliant Code Example
+
+```javascript
+function ms(hours, minutes, seconds) {
+  const duration = ((hours * 60 + minutes) * 60 + seconds) * 1000;
+  return duration;
+}
+```
+
+## Compliant Solution
+
+```javascript
+function ms(hours, minutes, seconds) {
+  return ((hours * 60 + minutes) * 60 + seconds) * 1000;
+}
+```
---
+++ src/rules/prefer-immediate-return.ts
@@ -0,0 +1,65 @@
+import { Rule } from "eslint";
+import * as estree from "estree";
+import { isReturnStatement, isThrowStatement, isIdentifier, isVariableDeclaration } from "../utils/nodes";
+
+const rule: Rule.RuleModule = {
+  create(context: Rule.RuleContext) {
+    return {
+      BlockStatement(node: estree.Node) {
+        processStatements((node as estree.BlockStatement).body);
+      },
+      SwitchCase(node: estree.Node) {
+        processStatements((node as estree.SwitchCase).consequent);
+      },
+    };
+
+    function processStatements(statements: estree.Statement[]) {
+      if (statements.length > 1) {
+        const last = statements[statements.length - 1];
+        const returnedIdentifier = getOnlyReturnedVariable(last);
+
+        const lastButOne = statements[statements.length - 2];
+        const declaredIdentifier = getOnlyDeclaredVariable(lastButOne);
+
+        if (returnedIdentifier && declaredIdentifier) {
+          const isSameVariable = context.getScope().variables.some(variable => {
+            return (
+              variable.references.find(ref => ref.identifier === returnedIdentifier) !== undefined &&
+              variable.references.find(ref => ref.identifier === declaredIdentifier.id) !== undefined
+            );
+          });
+
+          if (isSameVariable) {
+            context.report({
+              message: formatMessage(last, returnedIdentifier.name),
+              node: declaredIdentifier.init,
+            });
+          }
+        }
+      }
+    }
+
+    function getOnlyReturnedVariable(node: estree.Statement) {
+      return (isReturnStatement(node) || isThrowStatement(node)) && node.argument && isIdentifier(node.argument)
+        ? node.argument
+        : undefined;
+    }
+
+    function getOnlyDeclaredVariable(node: estree.Statement) {
+      if (isVariableDeclaration(node) && node.declarations.length === 1) {
+        const { id, init } = node.declarations[0];
+        if (isIdentifier(id) && init) {
+          return { id, init };
+        }
+      }
+      return undefined;
+    }
+
+    function formatMessage(node: estree.Node, variable: string) {
+      const action = isReturnStatement(node) ? "return" : "throw";
+      return `Immediately ${action} this expression instead of assigning it to the temporary variable "${variable}".`;
+    }
+  },
+};
+
+export = rule;
