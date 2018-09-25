0d9e6633c30f5ec875b759e602d1de6f683382db
Andrea Guarino <andrea.guarino@sonarsource.com>
2018-09-25 10:49:14 +0200

    add no-useless-catch rule

---
+++ src/rules/no-useless-catch.ts
@@ -0,0 +1,50 @@
+/*
+ * eslint-plugin-sonarjs
+ * Copyright (C) 2018 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+// https://jira.sonarsource.com/browse/RSPEC-1940
+
+import { Rule, SourceCode } from "eslint";
+import { Node, CatchClause, Statement, Pattern } from "estree";
+import { isThrowStatement } from "../utils/nodes";
+import { areEquivalent } from "../utils/equivalence";
+
+const MESSAGE = "Add logic to this catch clause or eliminate it and rethrow the exception automatically.";
+
+const rule: Rule.RuleModule = {
+  create(context: Rule.RuleContext) {
+    return { CatchClause: (node: Node) => visitCatchClause(node as CatchClause, context) };
+  },
+};
+
+function visitCatchClause(catchClause: CatchClause, context: Rule.RuleContext) {
+  const statements = catchClause.body.body;
+  if (statements.length === 1 && onlyRethrows(statements[0], catchClause.param, context.getSourceCode())) {
+    const catchKeyword = context.getSourceCode().getFirstToken(catchClause);
+    context.report({
+      message: MESSAGE,
+      loc: catchKeyword!.loc,
+    });
+  }
+}
+
+function onlyRethrows(statement: Statement, catchParam: Pattern, sourceCode: SourceCode) {
+  return isThrowStatement(statement) && areEquivalent(catchParam, statement.argument, sourceCode);
+}
+
+export = rule;
---
+++ tests/rules/no-useless-catch.test.ts
@@ -0,0 +1,85 @@
+/*
+ * eslint-plugin-sonarjs
+ * Copyright (C) 2018 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+import { RuleTester } from "eslint";
+
+const ruleTester = new RuleTester();
+import rule = require("../../src/rules/no-useless-catch");
+
+ruleTester.run("no-useless-catch", rule, {
+  valid: [
+    { code: `try {} catch (e) {}` },
+    {
+      code: `try {} catch (e) {
+              foo();
+              throw e;
+            }`,
+    },
+    {
+      code: `try {} catch (e) {
+              if (x) {
+                throw e;
+              }
+            }`,
+    },
+    {
+      code: `try {} catch(e) { throw "foo"; }`,
+    },
+    {
+      code: `try {} catch(e) { throw new Error("improve error message"); }`,
+    },
+  ],
+  invalid: [
+    {
+      code: `try {} catch (e) { throw e; }`,
+      errors: [
+        {
+          message: "Add logic to this catch clause or eliminate it and rethrow the exception automatically.",
+          line: 1,
+          endLine: 1,
+          column: 8,
+          endColumn: 13,
+        },
+      ],
+    },
+    {
+      code: `try {} catch(e) {
+        // some comment
+        throw e;
+      }`,
+      errors: 1,
+    },
+    {
+      code: `try {} catch({ message }) {
+        throw { message };
+      }`,
+      errors: 1,
+    },
+    {
+      code: `try {
+        doSomething();
+      } catch(e) {
+        throw e;
+      } finally {
+        // ...
+      }`,
+      errors: 1,
+    },
+  ],
+});
