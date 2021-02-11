1ea546e8550000bd15b887e00d848c18f5587a07
yassin-kammoun-sonarsource <yassin.kammoun@sonarsource.com>
2021-02-11 18:28:27 +0100

    Update CI test script

--- scripts/test-ci.sh
+++ scripts/test-ci.sh
@@ -6,8 +6,6 @@ set -euo pipefail
 # this is required for sonarcloud analysis
 if [ "${TRAVIS_NODE_VERSION}" = "10" ]; then
   echo 'Running tests with coverage and reporter'
-  # install `jest-sonar-reporter` here, because otherwise `yarn install` fails on nodejs
-  yarn add jest-sonar-reporter@2.0.0
   yarn test --coverage --testResultsProcessor jest-sonar-reporter
 else
   echo 'Running tests'
