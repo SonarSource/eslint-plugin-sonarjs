27c072d29fd125ba9abe7c129a92cf52ef667137
Tom <thomas.verin@sonarsource.com>
2021-04-06 11:42:00 +0200

    Run coverage only on node 10

--- scripts/test-ci.sh
+++ scripts/test-ci.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 # run tests with coverage and reports only for nodejs 10
 # this is required for sonarcloud analysis
-if [ "${TRAVIS_NODE_VERSION}" = "10" ]; then
+if [ "${NODE_VERSION}" == "v10*" ]; then
   echo 'Running tests with coverage and reporter'
   yarn test --coverage --testResultsProcessor jest-sonar-reporter
 else
