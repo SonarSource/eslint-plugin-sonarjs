43d8ffdf61746caeaffb2da4811a45fd2d9d6423
Tom <thomas.verin@sonarsource.com>
2021-04-07 11:04:33 +0200

    Only set version for deployable artifacts

--- scripts/analyze_and_publish.sh
+++ scripts/analyze_and_publish.sh
@@ -8,6 +8,10 @@ fi
 
 set -euo pipefail
 
+export VERSION=$(grep version package.json | head -1  | awk -F: '{ print $2 }' | sed 's/[",]//g')
+source cirrus-env BUILD
+npm version --no-git-tag-version $VERSION-$BUILD_NUMBER
+
 sonar-scanner \
   -Dsonar.organization=sonarsource \
   -Dsonar.host.url=https://sonarcloud.io
--- scripts/set-version.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export VERSION=$(grep version package.json | head -1  | awk -F: '{ print $2 }' | sed 's/[",]//g')
-source cirrus-env BUILD
-npm version --no-git-tag-version $VERSION-$BUILD_NUMBER
