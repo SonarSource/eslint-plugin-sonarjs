c7dc516c43189479c974ef045d064902772b8e88
Tom <thomas.verin@sonarsource.com>
2021-04-07 11:01:21 +0200

    Only publish the analysed artifacts

--- scripts/analyze_and_publish.sh
+++ scripts/analyze_and_publish.sh
@@ -2,7 +2,7 @@
 
 # analyze only on one axis of node versions, variable is set in dockerfile
 if [ ! "${SONARCLOUD_ANALYSIS:-}" == "true" ]; then
-  echo 'Analysis ignored'
+  echo 'Analysis and publish ignored'
   exit 0
 fi
 
@@ -11,3 +11,5 @@ set -euo pipefail
 sonar-scanner \
   -Dsonar.organization=sonarsource \
   -Dsonar.host.url=https://sonarcloud.io
+
+npm publish --registry https://repox.jfrog.io/artifactory/api/npm/sonarsource-npm-public/
