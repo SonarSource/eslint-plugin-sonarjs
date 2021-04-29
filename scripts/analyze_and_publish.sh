428278a9cd890348d8161af5ff02074dc394f6a0
tomverin <thomas.verin@sonarsource.com>
2021-04-29 14:44:02 +0200

    Update name to fit semver spec (#210)
    * Update name to fit semver spec
    
    * Fix set version with --allow-same-version

--- scripts/analyze_and_publish.sh
+++ scripts/analyze_and_publish.sh
@@ -10,7 +10,7 @@ set -euo pipefail
 
 export VERSION=$(grep version package.json | head -1  | awk -F: '{ print $2 }' | sed 's/[",]//g')
 source cirrus-env BUILD
-npm version --no-git-tag-version $VERSION-$BUILD_NUMBER
+npm version --no-git-tag-version --allow-same-version $VERSION+$BUILD_NUMBER
 
 sonar-scanner \
   -Dsonar.organization=sonarsource \
