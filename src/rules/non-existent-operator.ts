9b9c682b2ab341c4b3218b4c844123a497a3aa1e
Yassin Kammoun <52890329+yassin-kammoun-sonarsource@users.noreply.github.com>
2021-06-29 11:57:51 +0200

    Rule S2757: Non-existent operators '=+', '=-' and '=!' should not be used (#237)
    * Rule S2757: Non-existent operators '=+', '=-' and '=!' should not be used
    
    * rebase and fix types
    
    Co-authored-by: Tibor Blenessy <tibor.blenessy@sonarsource.com>

---
+++ src/rules/non-existent-operator.ts
@@ -0,0 +1,84 @@
+/*
+ * eslint-plugin-sonarjs
+ * Copyright (C) 2018-2021 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+// https://jira.sonarsource.com/browse/RSPEC-2757
+
+import { TSESLint, TSESTree } from '@typescript-eslint/experimental-utils';
+import { Rule } from '../utils/types';
+
+const rule: Rule.RuleModule = {
+  meta: {
+    type: 'problem',
+  },
+  create(context) {
+    return {
+      AssignmentExpression(node: TSESTree.Node) {
+        const assignmentExpression = node as TSESTree.AssignmentExpression;
+        if (assignmentExpression.operator === '=') {
+          checkOperator(context, assignmentExpression.right);
+        }
+      },
+      VariableDeclarator(node: TSESTree.Node) {
+        const variableDeclarator = node as TSESTree.VariableDeclarator;
+        checkOperator(context, variableDeclarator.init);
+      },
+    };
+  },
+};
+
+function checkOperator(context: Rule.RuleContext, unaryNode?: TSESTree.Expression | null) {
+  if (
+    unaryNode &&
+    unaryNode.type === 'UnaryExpression' &&
+    isUnaryOperatorOfInterest(unaryNode.operator)
+  ) {
+    const sourceCode = context.getSourceCode();
+    const assignmentOperatorToken = sourceCode.getTokenBefore(
+      unaryNode,
+      token => token.value === '=',
+    );
+    const unaryOperatorToken = sourceCode.getFirstToken(unaryNode);
+    const expressionFirstToken = sourceCode.getFirstToken(unaryNode.argument);
+
+    if (
+      assignmentOperatorToken != null &&
+      unaryOperatorToken != null &&
+      expressionFirstToken != null &&
+      areAdjacent(assignmentOperatorToken, unaryOperatorToken) &&
+      !areAdjacent(unaryOperatorToken, expressionFirstToken)
+    ) {
+      context.report({
+        message: `Was "${unaryNode.operator}=" meant instead?`,
+        loc: { start: assignmentOperatorToken.loc.start, end: unaryOperatorToken.loc.end },
+      });
+    }
+  }
+}
+
+function isUnaryOperatorOfInterest(operator: TSESTree.UnaryExpression['operator']): boolean {
+  return operator === '-' || operator === '+' || operator === '!';
+}
+
+function areAdjacent(first: TSESLint.AST.Token, second: TSESLint.AST.Token): boolean {
+  return (
+    first.loc.end.column === second.loc.start.column && first.loc.end.line === second.loc.start.line
+  );
+}
+
+export = rule;
---
+++ tests/rules/non-existent-operator.test.ts
@@ -0,0 +1,106 @@
+/*
+ * eslint-plugin-sonarjs
+ * Copyright (C) 2018-2021 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+import { ruleTester } from '../rule-tester';
+import * as rule from '../../src/rules/non-existent-operator';
+
+ruleTester.run("Non-existent operators '=+', '=-' and '=!' should not be used", rule, {
+  valid: [
+    {
+      code: `
+        x = y;
+        x += y;
+        x = + y;
+        x =
+           + y;
+        x=+y; // Ok - we accept this as some people don't like to use white spaces
+        x = - y;
+        x /=+ y;
+        x = !y;
+        let y = + 1;
+        y = (!(y));
+        const z = + 1;
+        other =~ 1;
+        `,
+    },
+  ],
+  invalid: [
+    {
+      code: `x =+ y;`,
+      errors: [
+        {
+          message: `Was "+=" meant instead?`,
+          line: 1,
+          endLine: 1,
+          column: 3,
+          endColumn: 5,
+        },
+      ],
+    },
+    {
+      code: `
+      x =- y;`,
+      errors: [
+        {
+          message: `Was "-=" meant instead?`,
+          line: 2,
+          endLine: 2,
+          column: 9,
+          endColumn: 11,
+        },
+      ],
+    },
+    {
+      code: `x =! y;`,
+      errors: [
+        {
+          message: `Was "!=" meant instead?`,
+          line: 1,
+          endLine: 1,
+          column: 3,
+          endColumn: 5,
+        },
+      ],
+    },
+    {
+      code: `const x =! y;`,
+      errors: [
+        {
+          message: `Was "!=" meant instead?`,
+          line: 1,
+          endLine: 1,
+          column: 9,
+          endColumn: 11,
+        },
+      ],
+    },
+    {
+      code: `let x =! y;`,
+      errors: [
+        {
+          message: `Was "!=" meant instead?`,
+          line: 1,
+          endLine: 1,
+          column: 7,
+          endColumn: 9,
+        },
+      ],
+    },
+  ],
+});
