38cb0f3e43ed82a918706c8a1b1123c7082f2bbd
yassin-kammoun-sonarsource <yassin.kammoun@sonarsource.com>
2021-06-30 18:59:48 +0200

    Replace Jira-based RSPEC links with new repository-based RSPEC links

--- src/rules/generator-without-yield.ts
+++ src/rules/generator-without-yield.ts
@@ -17,6 +17,7 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
+// https://sonarsource.github.io/rspec/#/rspec/S3531
 
 import { TSESTree } from '@typescript-eslint/experimental-utils';
 import { getMainFunctionTokenLocation } from '../utils/locations';
--- src/rules/no-identical-conditions.ts
+++ src/rules/no-identical-conditions.ts
@@ -17,7 +17,8 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-// https://jira.sonarsource.com/browse/RSPEC-1862
+// https://sonarsource.github.io/rspec/#/rspec/S1862
+
 import { TSESTree } from '@typescript-eslint/experimental-utils';
 import { Rule } from '../utils/types';
 import { isIfStatement } from '../utils/nodes';
