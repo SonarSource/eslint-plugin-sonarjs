7eaabbea03dc7f65030b7ccc1139da6f42de9d62
vilchik.elena <elena.vilchik@sonarsource.com>
2021-07-05 17:30:36 +0200

    Fix some things

--- scripts/run_integration_test.sh
+++ scripts/run_integration_test.sh
@@ -1,6 +1,12 @@
+#!/bin/bash
+
+set -euo pipefail
+
+export VERSION=$(grep version package.json | head -1  | awk -F: '{ print $2 }' | sed 's/[",]//g')
+source cirrus-env BUILD
+
 rm -rf node_modules
 cd integration-test-project
 npm install
-export VERSION=$(grep version package.json | head -1  | awk -F: '{ print $2 }' | sed 's/[",]//g')
 npm install --registry https://repox.jfrog.io/artifactory/api/npm/sonarsource-npm-public/ eslint-plugin-sonarjs@$VERSION-$BUILD_NUMBER
 npm run eslint -- index.ts
