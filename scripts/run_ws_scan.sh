35dc9af6356e7e1b966bb253ca80095b0b12173a
Tibor Blenessy <tibor.blenessy@sonarsource.com>
2021-11-22 16:03:25 +0100

    Fix WhiteSource project name

--- scripts/run_ws_scan.sh
+++ scripts/run_ws_scan.sh
@@ -2,8 +2,7 @@
 
 export VERSION=$(grep version package.json | head -1  | awk -F: '{ print $2 }' | sed 's/[",]//g')
 
-export WS_PRODUCTNAME="org.sonarsource.sonarjs:eslint-plugin-sonarjs"
-export WS_PROJECTNAME="org.sonarsource.sonarjs:eslint-plugin-sonarjs ${VERSION}"
+export WS_PROJECTNAME="${CIRRUS_REPO_FULL_NAME} ${VERSION}"
 export BUILD_NUMBER=${VERSION}
 
 source ws_scan.sh
