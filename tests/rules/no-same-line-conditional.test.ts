a99dda37f4f6fa0e35c8012aeb750cfcc9083c65
yassin-kammoun-sonarsource <yassin.kammoun@sonarsource.com>
2021-11-22 16:31:25 +0100

    Disable linting issues

--- tests/rules/no-same-line-conditional.test.ts
+++ tests/rules/no-same-line-conditional.test.ts
@@ -17,8 +17,8 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
+/* eslint-disable sonarjs/no-duplicate-string, import/newline-after-import */
 import { ruleTester } from '../rule-tester';
-
 import rule = require('../../src/rules/no-same-line-conditional');
 
 ruleTester.run('Conditionals should start on new lines', rule, {
