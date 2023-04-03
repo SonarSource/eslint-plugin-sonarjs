d330c9bd903d879ef822d2e13582f5de3759c064
Ilia Kebets <ilia.kebets@sonarsource.com>
2023-04-03 18:06:05 +0200

    evert "include rule implementation of typescript-eslint's "no-confusing-non-null-assertion""
    This reverts commit c6faa227f4ac237bd21492153c94559240e2ea15.

--- tests/rules/non-existent-operator.test.ts
+++ tests/rules/non-existent-operator.test.ts
@@ -20,7 +20,7 @@
 import { ruleTester } from '../rule-tester';
 import * as rule from '../../src/rules/non-existent-operator';
 
-ruleTester.run("Confusing and non-existent operators should not be used", rule, {
+ruleTester.run("Non-existent operators '=+', '=-' and '=!' should not be used", rule, {
   valid: [
     {
       code: `
@@ -39,12 +39,6 @@ ruleTester.run("Confusing and non-existent operators should not be used", rule, 
         other =~ 1;
         `,
     },
-    'a == b!;',
-    'a = b!;',
-    'a !== b;',
-    'a != b;',
-    '(a + b!) == c;',
-    '(a + b!) = c;',
   ],
   invalid: [
     {
@@ -130,134 +124,6 @@ ruleTester.run("Confusing and non-existent operators should not be used", rule, 
           column: 7,
           endColumn: 9,
           suggestions: [],
-        },
-      ],
-    },
-    {
-      code: 'a! == b;',
-      errors: [
-        {
-          messageId: 'confusingEqual',
-          line: 1,
-          column: 1,
-          suggestions: [
-            {
-              messageId: 'notNeedInEqualTest',
-              output: 'a == b;',
-            },
-          ],
-        },
-      ],
-    },
-    {
-      code: 'a! === b;',
-      errors: [
-        {
-          messageId: 'confusingEqual',
-          line: 1,
-          column: 1,
-          suggestions: [
-            {
-              messageId: 'notNeedInEqualTest',
-              output: 'a === b;',
-            },
-          ],
-        },
-      ],
-    },
-    {
-      code: 'a + b! == c;',
-      errors: [
-        {
-          messageId: 'confusingEqual',
-          line: 1,
-          column: 1,
-          suggestions: [
-            {
-              messageId: 'wrapUpLeft',
-              output: '(a + b!) == c;',
-            },
-          ],
-        },
-      ],
-    },
-    {
-      code: '(obj = new new OuterObj().InnerObj).Name! == c;',
-      errors: [
-        {
-          messageId: 'confusingEqual',
-          line: 1,
-          column: 1,
-          suggestions: [
-            {
-              messageId: 'notNeedInEqualTest',
-              output: '(obj = new new OuterObj().InnerObj).Name == c;',
-            },
-          ],
-        },
-      ],
-    },
-    {
-      code: '(a==b)! ==c;',
-      errors: [
-        {
-          messageId: 'confusingEqual',
-          line: 1,
-          column: 1,
-          suggestions: [
-            {
-              messageId: 'notNeedInEqualTest',
-              output: '(a==b) ==c;',
-            },
-          ],
-        },
-      ],
-    },
-    {
-      code: 'a! = b;',
-      errors: [
-        {
-          messageId: 'confusingAssign',
-          line: 1,
-          column: 1,
-          suggestions: [
-            {
-              messageId: 'notNeedInAssign',
-              output: 'a = b;',
-            },
-          ],
-        },
-      ],
-    },
-    {
-      code: '(obj = new new OuterObj().InnerObj).Name! = c;',
-      errors: [
-        {
-          messageId: 'confusingAssign',
-          line: 1,
-          column: 1,
-          suggestions: [
-            {
-              messageId: 'notNeedInAssign',
-              output: '(obj = new new OuterObj().InnerObj).Name = c;',
-            },
-          ],
-        },
-      ],
-    },
-    {
-      code: '(a=b)! =c;',
-      errors: [
-        {
-          messageId: 'confusingAssign',
-          line: 1,
-          column: 1,
-          suggestions: [
-            {
-              messageId: 'notNeedInAssign',
-              output: '(a=b) =c;',
-            },
-          ],
         },
       ],
     },
