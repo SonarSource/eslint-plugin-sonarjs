b249dcb4defe8de9b6e61420ab32525942ace2b5
Michal Zgliczynski <michal.zgliczynski@sonarsource.com>
2024-03-08 15:33:23 +0100

    change workers

--- scripts/test-ci.sh
+++ scripts/test-ci.sh
@@ -7,7 +7,7 @@ set -euo pipefail
 # variable is set in dockerfile
 if [ "${SONARCLOUD_ANALYSIS:-}" == "true" ]; then
   echo 'Running tests with coverage and reporter'
-  npm run test -- --coverage --testResultsProcessor jest-sonar-reporter
+  npm run test -- --coverage --testResultsProcessor --maxWorkers=50% jest-sonar-reporter
 else
   echo 'Running tests'
   npm run test
